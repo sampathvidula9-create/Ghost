2b3bbb094588de075d598531dd9b3539067eb75a
sampathvidula9-create <sampathvidula9@gmail.com>
2026-05-18 13:50:47 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -29,10 +29,10 @@ cmd(
         });
       }
 
-      let menuText = "📋 *Available Commands:*\n";
+      let menuText = "👾 *Available Commands👹:*\n";
 
       for (const [cat, cmds] of Object.entries(categories)) {
-        menuText += `\n📂 *${cat.toUpperCase()}*\n`;
+        menuText += `\n👾 *${cat.toUpperCase()}*\n`;
         cmds.forEach(c => {
           menuText += `- .${c.pattern} : ${c.desc}\n`;
         });
